04f6e1c12769673f558dc9e3f44bf776c1b0b3f0
Oleksandr Akhtyrskiy <lex57ukr@gmail.com>
2025-06-02 23:41:26 -0600

    Add `DependenciesInstallerTests`

---
+++ tests/Keystone.Cli.UnitTests/Configuration/DependenciesInstallerTests.cs
@@ -0,0 +1,49 @@
+using Keystone.Cli.Application;
+using Keystone.Cli.Application.Commands.Browse;
+using Keystone.Cli.Application.Commands.Info;
+using Keystone.Cli.Application.Commands.New;
+using Keystone.Cli.Application.Data;
+using Keystone.Cli.Application.Utility;
+using Keystone.Cli.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Keystone.Cli.UnitTests.Configuration;
+
+[TestFixture, Parallelizable(ParallelScope.All)]
+public class DependenciesInstallerTests
+{
+    private static readonly Type[] ExpectedTypes =
+    [
+        typeof(ITemplateService),
+        typeof(ITemplateTargetsRepository),
+        typeof(IProcessService),
+        typeof(IBrowseCommand),
+        typeof(IInfoCommand),
+        typeof(INewCommand),
+    ];
+
+    [TestCaseSource(nameof(ExpectedTypes))]
+    public void ResolvesExpectedTypes(Type type)
+    {
+        var settings = new Dictionary<string, string>
+        {
+            ["Templates:core"] = "https://github.com/knight-owl-dev/keystone-template-core",
+            ["Templates:core-slim"] = "https://github.com/knight-owl-dev/keystone-template-core-slim",
+            ["DefaultTemplate"] = "core-slim",
+        };
+
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(settings)
+            .Build();
+
+        var services = new ServiceCollection();
+        services.AddSingleton<IConfiguration>(configuration);
+        services.AddDependencies();
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        Assert.That(() => serviceProvider.GetService(type), Throws.Nothing);
+    }
+}
