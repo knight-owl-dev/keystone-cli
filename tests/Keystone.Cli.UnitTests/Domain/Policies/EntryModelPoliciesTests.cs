7c0b6890a64633f1d07e721368cead031cf58121
Oleksandr Akhtyrskiy <lex57ukr@gmail.com>
2025-07-30 23:57:48 -0600

    Add a remarks section to explain the partial tests structure

--- tests/Keystone.Cli.UnitTests/Domain/Policies/EntryModelPoliciesTests.cs
+++ tests/Keystone.Cli.UnitTests/Domain/Policies/EntryModelPoliciesTests.cs
@@ -1,4 +1,12 @@
+using Keystone.Cli.Domain.Policies;
+
+
 namespace Keystone.Cli.UnitTests.Domain.Policies;
 
+/// <remarks>
+/// Test individual policies from <see cref="EntryModelPolicies"/> in partial classes
+/// for better organization, e.g., <c>EntryModelPoliciesTests.PolicyMethodName.cs</c>,
+/// where <c>PolicyMethodName</c> is the name of the policy method being tested.
+/// </remarks>
 [TestFixture, Parallelizable(ParallelScope.All)]
 public partial class EntryModelPoliciesTests;
