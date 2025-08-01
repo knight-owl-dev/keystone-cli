d3c7bd30e2006f3984c39e3b3d69d4fa51e03f8a
Oleksandr Akhtyrskiy <lex57ukr@gmail.com>
2025-07-31 19:58:53 -0600

    Add `PathAttributeTests`

---
+++ tests/Keystone.Cli.UnitTests/Presentation/ComponentModel/DataAnnotations/PathAttributeTests.cs
@@ -0,0 +1,82 @@
+using Keystone.Cli.Presentation.ComponentModel.DataAnnotations;
+
+
+namespace Keystone.Cli.UnitTests.Presentation.ComponentModel.DataAnnotations;
+
+[TestFixture, Parallelizable(ParallelScope.All)]
+public class PathAttributeTests
+{
+    private static readonly TestCaseData<string>[] ValidPathTestCases =
+    [
+        new("."),
+        new("~"),
+        new("path/to/directory"),
+        new("/path/to/directory"),
+        new(@"C:\path\to\directory"),
+        new("./path/to/directory"),
+        new("~/path/to/directory"),
+        new("./path/to/directory/"),
+    ];
+
+    private static readonly TestCaseData<string>[] InvalidPathTestCases =
+    [
+        new(string.Empty),
+        new(" "),
+        new(" ./path/to/directory "),
+        ..Path.GetInvalidPathChars().Select(c => new TestCaseData<string>($"./path/to/directory/{c}")),
+    ];
+
+    [Test]
+    public void IsValid_WhenValueIsNull_ReturnsTrue()
+    {
+        var sut = new PathAttribute();
+        var actual = sut.IsValid(value: null);
+
+        Assert.That(actual, Is.True);
+    }
+
+    [TestCaseSource(nameof(ValidPathTestCases))]
+    public void IsValid_WhenValueIsValid_ReturnsTrue(string path)
+    {
+        var sut = new PathAttribute();
+        var actual = sut.IsValid(path);
+
+        Assert.That(actual, Is.True);
+    }
+
+    [TestCaseSource(nameof(InvalidPathTestCases))]
+    public void IsValid_WhenValueIsInvalid_ReturnsFalse(string path)
+    {
+        var sut = new PathAttribute();
+        var actual = sut.IsValid(string.Empty);
+
+        Assert.That(actual, Is.False);
+    }
+
+    [Test]
+    public void FormatErrorMessage_WhenHasErrorMessage_ReturnsErrorMessage()
+    {
+        const string name = "TestPath";
+        const string errorMessage = "Custom error message.";
+
+        var sut = new PathAttribute
+        {
+            ErrorMessage = errorMessage,
+        };
+
+        var actual = sut.FormatErrorMessage(name);
+
+        Assert.That(actual, Is.EqualTo(errorMessage));
+    }
+
+    [Test]
+    public void FormatErrorMessage_WhenNoErrorMessage_ReturnsDefaultErrorMessage()
+    {
+        const string name = "TestPath";
+
+        var sut = new PathAttribute();
+        var actual = sut.FormatErrorMessage(name);
+
+        Assert.That(actual, Is.EqualTo($"The {name} field must be a valid path."));
+    }
+}
