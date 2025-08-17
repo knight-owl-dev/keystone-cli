eedd70d5a4e39ed896696fcc00ae32a76c9ddebe
Oleksandr Akhtyrskiy <lex57ukr@gmail.com>
2025-08-16 20:17:40 -0600

    Wip `ProjectCommand`
    Add logger and template service dependencies.

--- src/Keystone.Cli/Application/Commands/Project/ProjectCommand.cs
+++ src/Keystone.Cli/Application/Commands/Project/ProjectCommand.cs
@@ -1,12 +1,29 @@
+using Microsoft.Extensions.Logging;
+
+
 namespace Keystone.Cli.Application.Commands.Project;
 
 /// <summary>
 /// The default implementation of <see cref="IProjectCommand"/> for managing Keystone projects.
 /// </summary>
-public class ProjectCommand
+public class ProjectCommand(ILogger<ProjectCommand> logger, ITemplateService templateService)
     : IProjectCommand
 {
     /// <inheritdoc />
     public Task<bool> SwitchTemplateAsync(string newTemplateName, string projectPath, CancellationToken cancellationToken)
-        => Task.FromResult(false);
+    {
+        // TODO: read from the project metadata
+        var projectName = Path.GetFileName(projectPath);
+
+        var templateTarget = templateService.GetTemplateTarget(newTemplateName);
+
+        logger.LogInformation(
+            "Switching project '{ProjectName}' to use {RepositoryUrl} template in {Path}",
+            projectName,
+            templateTarget.RepositoryUrl,
+            projectPath
+        );
+
+        return Task.FromResult(false);
+    }
 }
--- tests/Keystone.Cli.UnitTests/Application/Commands/Project/ProjectCommandTests.cs
+++ tests/Keystone.Cli.UnitTests/Application/Commands/Project/ProjectCommandTests.cs
@@ -1,4 +1,9 @@
+using Keystone.Cli.Application;
 using Keystone.Cli.Application.Commands.Project;
+using Keystone.Cli.Domain;
+using Keystone.Cli.UnitTests.Logging;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
 
 
 namespace Keystone.Cli.UnitTests.Application.Commands.Project;
@@ -6,8 +11,11 @@ namespace Keystone.Cli.UnitTests.Application.Commands.Project;
 [TestFixture, Parallelizable(ParallelScope.All)]
 public class ProjectCommandTests
 {
-    private static ProjectCommand Ctor()
-        => new();
+    private static ProjectCommand Ctor(ILogger<ProjectCommand>? logger = null, ITemplateService? templateService = null)
+        => new(
+            logger ?? new TestLogger<ProjectCommand>(),
+            templateService ?? Substitute.For<ITemplateService>()
+        );
 
     [Test]
     public async Task SwitchTemplateAsync_NoChanges_ReturnsFalseAsync()
@@ -15,7 +23,15 @@ public class ProjectCommandTests
         const string newTemplateName = "new-template";
         const string projectPath = "/path/to/project";
 
-        var sut = Ctor();
+        var templateTarget = new TemplateTargetModel(
+            Name: newTemplateName,
+            RepositoryUrl: new Uri("https://github.com/knight-owl-dev/template-a")
+        );
+
+        var templateService = Substitute.For<ITemplateService>();
+        templateService.GetTemplateTarget(newTemplateName).Returns(templateTarget);
+
+        var sut = Ctor(templateService: templateService);
 
         var actual = await sut.SwitchTemplateAsync(newTemplateName, projectPath, CancellationToken.None);
 
