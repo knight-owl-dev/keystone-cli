0853e1846ab5598e2b9450b04d20277e20c4d1de
Oleksandr Akhtyrskiy <lex57ukr@gmail.com>
2026-01-11 17:12:45 -0700

    Bump version to `0.1.2` and update related documentation (#9)

--- tests/Keystone.Cli.UnitTests/Application/Commands/Info/InfoCommandTests.cs
+++ tests/Keystone.Cli.UnitTests/Application/Commands/Info/InfoCommandTests.cs
@@ -33,7 +33,7 @@ public class InfoCommandTests
 
         using (Assert.EnterMultipleScope())
         {
-            Assert.That(actual.Version, Does.StartWith("0.1.1"));
+            Assert.That(actual.Version, Does.StartWith("0.1.2"));
             Assert.That(actual.Description, Is.EqualTo("A command-line interface for Keystone."));
             Assert.That(actual.Copyright, Is.EqualTo("© 2025 Knight Owl LLC. All rights reserved."));
             Assert.That(actual.DefaultTemplateTarget, Is.EqualTo(defaultTemplateTarget));
