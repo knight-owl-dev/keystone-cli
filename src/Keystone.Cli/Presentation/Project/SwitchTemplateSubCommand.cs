c03e3ba856fac65ea4e758bf9cb7628f2dfc7b18
Oleksandr Akhtyrskiy <lex57ukr@gmail.com>
2026-01-18 05:39:01 -0700

    Refactor `SwitchTemplateSubCommand` to use `CancellationToken` from context accessor

--- src/Keystone.Cli/Presentation/Project/SwitchTemplateSubCommand.cs
+++ src/Keystone.Cli/Presentation/Project/SwitchTemplateSubCommand.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using Cocona;
+using Cocona.Application;
 using Keystone.Cli.Application.Commands.Project;
 using Keystone.Cli.Application.Utility;
 using Keystone.Cli.Domain;
@@ -12,7 +13,11 @@ namespace Keystone.Cli.Presentation.Project;
 /// <summary>
 /// The implementation of the "switch-template" sub-command for the project command.
 /// </summary>
-public class SwitchTemplateSubCommand(IConsole console, IProjectCommand projectCommand)
+public class SwitchTemplateSubCommand(
+    ICoconaAppContextAccessor contextAccessor,
+    IConsole console,
+    IProjectCommand projectCommand
+)
 {
     public async Task<int> SwitchTemplateAsync(
         [Argument(Description = "The name of the new template to switch to"),
@@ -20,10 +25,11 @@ public class SwitchTemplateSubCommand(IConsole console, IProjectCommand projectC
         string newTemplateName,
         [Option(Description = "The path to the project where the template should be switched"),
          Path, NotPaddedWhitespace]
-        string? projectPath = null,
-        CancellationToken cancellationToken = default
+        string? projectPath = null
     )
     {
+        var cancellationToken = contextAccessor.Current?.CancellationToken ?? CancellationToken.None;
+
         var fullPath = string.IsNullOrWhiteSpace(projectPath)
             ? Path.GetFullPath(".")
             : Path.GetFullPath(projectPath);
